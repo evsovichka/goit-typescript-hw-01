d47065c478a60bea43fc825d99c67748aac3ba79
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-10-08 00:39:12 +0300

    basic

---
+++ src/basic/3.ts
@@ -0,0 +1,7 @@
+// Створіть змінну, яка може містити або рядок, або число (union type)
+
+let mixedType: string | number;
+
+// Оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type).
+
+let isDisabled: "enable" | "disable";
---
+++ src/basic/7.ts
@@ -0,0 +1,25 @@
+type BasicPage = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: { createAt: Date; updateAt: Date };
+};
+
+const page1: BasicPage = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: BasicPage = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
